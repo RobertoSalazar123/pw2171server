ef0dd78b43c3d663bb8010397fd798377d0fcc95
RobertoSalazar123 <ccdm26@iMac-de-CCDM-26.local>
2017-05-24 15:05:43 -0600

    eventos.php-pendiente, datos.php valida en prueba

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -1,2 +1,21 @@
-var iniciaApp=function(){}
-$(document).on("ready",iniciaApp);
+var iniciaApp=function(){
+	var entrar=function(){
+		alert($("#txtusuario").val());
+		alert($("#txtclave").val());
+	}
+	var teclaUsuario=function(tecla){
+		if(tecla.which==13){
+			$("#txtclave").focus();
+		}
+	}
+	var teclaClave=function(tecla){
+		if(tecla.which==13){
+			entrar();
+		}
+	}
+	$("#btnentrar").on("click",entrar);
+	$("#txtusuario").on("keypress",teclaUsuario);
+	$("#txtclave").on("keypress",teclaClave);
+
+}
+$(document).ready(iniciaApp);
